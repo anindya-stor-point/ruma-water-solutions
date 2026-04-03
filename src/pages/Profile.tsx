f8ec6571c8f78f23cf20b463b65abd90b979dcd2
anindya-stor-point <haldpradip6@gmail.com>
2026-04-03 09:31:09 +0530

    'Redirected update button to default browser and integrated profile icon'

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -245,7 +245,7 @@ export default function Profile() {
           className="text-sm font-black text-gray-400 cursor-pointer"
           onClick={handleVersionTap}
         >
-          v1.0.17-stable
+          v1.0.18-stable
         </p>
       </div>
 
