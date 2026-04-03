8710ad917316f4f1cfe4f2de5c9f8a13194fa839
anindya-stor-point <haldpradip6@gmail.com>
2026-04-03 14:04:07 +0530

    'Re-provisioned Firebase and updated configuration to fix Firestore connection issues'

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -245,7 +245,7 @@ export default function Profile() {
           className="text-sm font-black text-gray-400 cursor-pointer"
           onClick={handleVersionTap}
         >
-          v1.0.22-stable
+          v1.0.23-stable
         </p>
       </div>
 
